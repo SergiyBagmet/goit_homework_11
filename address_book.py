ddcf7d3eb9326704189188f0f80b61767f2d3b5a
SergiyBagmet <134723713+SergiyBagmet@users.noreply.github.com>
2023-08-01 23:49:48 +0300

    __str__ + simple iter + valid(to classmetod)
    TODO iter with N

--- address_book.py
+++ address_book.py
@@ -7,24 +7,29 @@ class Field:
     поле для ввода новых данных в записную книгу
     """
     def __init__(self, value: str) -> None:
+        self.__valid_value(value)
+
         self._value = None
         self._value = value
       
-         
-    @staticmethod
-    def __valid_value(value) -> bool:
-        return type(value) == str 
-
+    @classmethod 
+    def __valid_value(cls, value) -> None:
+        if type(value) != str:
+            raise ValueError('received data must be STR') 
+        
     @property
     def value(self):
         return self._value
     
     @value.setter
-    def value(self, value: str):
-        if self.__valid_value(value) :
-           self._value = value
-        else:
-            raise ValueError('received data must be STR')    
+    def value(self, value: str) -> None:
+        self.__valid_value(value)
+        self._value = value
+
+    def __str__(self) -> str:
+        if self.value == None:
+            return f""
+        return f'{self.value}'
         
 
 class Name(Field):
@@ -33,6 +38,7 @@ class Name(Field):
     """
     def __init__(self, value: str) -> None:
         super().__init__(value)
+        # self.__valid_value(value)
 
 
 class Phone(Field):
@@ -40,54 +46,45 @@ class Phone(Field):
     поле номера телефона, принимает стр(только цифри)
     """
     def __init__(self, value: str) -> None:
-        if self.__valid_value(value):
-             super().__init__(value)
-        else:
+        super().__init__(value)
+        self.__valid_value(value)
+        
+      
+    @classmethod
+    def __valid_value(cls, value) -> None:
+        # тут нужна проверка на стр?
+        phone = ''.join(filter(str.isdigit, value))
+        if 9 >= len(phone) <= 15 : #псевдо проверка номера
             raise ValueError("Phone number isn't correct")    
 
-    @staticmethod
-    def __valid_value(value) -> bool:
-        if type(value) == str:  # TODO как тут сделать через супер?
-            phone = ''.join(filter(str.isdigit, value))
-            return 10 <= len(phone) <= 15 #псевдо проверка номера
-        else: 
-            raise ValueError('received data must be STR')    
-
     @Field.value.setter
-    def value(self, value: str): 
-        if self.__valid_value(value):
-            self._value = value
-        else:
-            raise ValueError("Phone number isn't correct") 
+    def value(self, value: str) -> None:
+        self.__valid_value(value)
+        self._value = value
+      
 
 class Birthday(Field):
     """
     поле дати рождения, принимает любую стр без проверки
     """
     def __init__(self, value: str) -> None:
-        if self.__valid_value(value):
-            super().__init__(value)
-        else:
-            raise ValueError('not correct date isoformat >>> yyyy-mm-dd')    
+        super().__init__(value)
+        self.__valid_value(value)
+        
+   
+    @classmethod
+    def __valid_value(cls, value) -> None:
+        # тут нужна проверка на стр?
+        try:
+            valid_date = date.fromisoformat(value)
+        except: # может тут сразу ошибку выдавать без райза???
+            raise ValueError('not correct date isoformat >>> yyyy-mm-dd')
             
-
-    @staticmethod
-    def __valid_value(value) -> bool:
-        if type(value) == str:
-            try:
-                valid_date = date.fromisoformat(value)
-                return True
-            except ValueError:
-                return False
-        else:
-            raise ValueError('received data must be STR')      
-
     @Field.value.setter
-    def value(self, value: str): 
-        if self.__valid_value(value):
-            self._value = value
-        else:      
-            raise ValueError('not correct date isoformat >>> yyyy-mm-dd') 
+    def value(self, value: str) -> None:
+        self.__valid_value(value)
+        self._value = value
+      
         
 class Record:
     """
@@ -95,10 +92,15 @@ class Record:
     сейчас простая реализация add/remove/change phone 
     """
   
-    def __init__(self, name: Name, phone: Phone=None, birthday: Birthday=None ) -> None:
+    def __init__(self, name: Name, phone: Phone, birthday: Birthday=None ) -> None:
         self.name = name
         self.phones = [phone] if phone else []
         self.birthday = birthday if birthday else None
+    
+    def __str__(self) -> str:# принтуем рекорд..не знаю как принято..сделал как чувствую
+        birthday_str = "birthday: "+str(self.birthday) if self.birthday != None else ""
+        phones_str = " ".join([ph.value for ph in self.phones]) 
+        return f'<Record> name: {self.name} -->> phone(s): {phones_str} {birthday_str}'
 
     def add_phone(self, phone: Phone):   
         self.phones.append(phone)
@@ -123,7 +125,7 @@ class Record:
         bday = date.fromisoformat(self.birthday.value).replace(year=today.year)
         if today > bday :
            bday= bday.replace(year=today.year+1)
-        return (bday-today).days
+        return (bday - today).days
       
     
 class AddressBook(UserDict):
@@ -133,27 +135,60 @@ class AddressBook(UserDict):
     """
 
     def add_record(self, rec: Record):
-        if not isinstance(rec, Record):
+        if type(rec) != Record:
             raise ValueError("Record must be an instance of the Record class.")
         self.data[rec.name.value] = rec
     
-    def __next__(self, n: int):
-        if not self.data:
-            raise StopIteration
-        pass 
+    def __iter__(self):
+        self.__counter = 0
+        return self
+
+    def __next__(self):
+        if self.__counter < len(self.data.keys()):
+            rec = [val for val in self.data.values()]
+            self.__counter +=1 
+            return rec[self.__counter - 1]
+        raise StopIteration
+    
+    def ierator(self, n):
+        pass
         
-    def __iter__(self, n: int):
-        return self.__next__(n)    
+ 
         
 if __name__ == '__main__':
 
-    name = Name('Bill')
-    phone = Phone('1234567890')
-    b_day = Birthday('1994-02-26')
-    rec = Record(name, phone, b_day)
-    print(rec.phones[0].value)
-    print(rec.birthday.value)
-    print(rec.days_to_birthday())
+    name_1 = Name('Bill')
+    phone_1 = Phone('1234567890')
+    b_day_1 = Birthday('1994-02-26')
+
+    name_2 = Name('serg')
+    phone_2 = Phone('1234567890')
+    b_day_2 = Birthday('1994-02-26')
+    
+    name_3 = Name('Oleg')
+    phone_3 = Phone('1234567890')
+    b_day_3 = Birthday('1994-02-26')
+
+
+    rec_1 = Record(name_1, phone_1, b_day_1)
+    rec_2 = Record(name_2, phone_2, b_day_2)
+    rec_3 = Record(name_3, phone_3, b_day_3)
+    ab = AddressBook()
+    ab.add_record(rec_1)
+    ab.add_record(rec_2)
+    ab.add_record(rec_3)
+
+    for i in ab:
+        print(i)
+
+    
+    # name = Name('Bill')
+    # phone = Phone('1234567890')
+    # b_day = Birthday('1994-02-26')
+    # rec = Record(name, phone, b_day)
+    # print(rec.phones[0].value)
+    # print(rec.birthday.value)
+    # print(rec.days_to_birthday())
     
     # name = Name('Bill')
     # phone = Phone('1234567890')
